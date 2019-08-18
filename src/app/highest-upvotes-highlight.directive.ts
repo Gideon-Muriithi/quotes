6533d980da9f11ddd7e5fece87fadcf28432e6c9
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-18 18:31:59 +0300

    This commit adds custom directive logic and function to highlight quotes with the highest upvotes

--- src/app/highest-upvotes-highlight.directive.ts
+++ src/app/highest-upvotes-highlight.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighestUpvotesHighlight]'
 })
 export class HighestUpvotesHighlightDirective {
+  constructor(private elem: ElementRef) { }
 
-  constructor() { }
-
+  @HostListener('click') upVotes() {
+    this.highlight('highlight');
+  }
+  @HostListener('dblclick') onDoubleClicks() {
+    this.highlight('highlight');
+  }
+  private highlight(highlight: string) {
+    this.elem.nativeElement.style.backgroundColor = '#87ceeb'; // style will be fill with yellow as bg color on click trigger
+  }
 }
