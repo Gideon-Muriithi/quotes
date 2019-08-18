1d5310d926aca85ddd51e54f4d74584eb763f53c
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-18 09:35:30 +0300

    This commit adds styling and elements that out quotes will have in quotes template class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,9 @@
 export class Quote {
+  showDescrption = false;
+  constructor(
+  public id: number,
+  public name: string,
+  public description: string,
+  public submssion: string,
+  public completeDate: Date ) {}
 }
