63bdf5c2128448f2195c4b4fb6bd02e5a100a8c1
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-18 08:29:52 +0300

    This commit adds child components to the root component and class quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { VotesComponent } from './votes/votes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    VotesComponent
   ],
   imports: [
     BrowserModule,
